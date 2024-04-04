65170314b404ef7d5c18bc2cdf6536d5fbd4d00d
Minjares <A01284650@tec.mx>
2024-04-04 13:53:53 -0600

    Initial config

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,20 @@
+return { 
+    "catppuccin/nvim",
+    name = "catppuccin", 
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin"
+      require("catppuccin").setup({
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        telescope = true
+    }
+
+})
+    end
+
+} 
+    
---
+++ lua/plugins/minintro.lua
@@ -0,0 +1,6 @@
+return {
+    "eoh-bse/minintro.nvim",
+     config = function()
+        require("minintro").setup({ color = "#bd93f9" })
+     end
+}
