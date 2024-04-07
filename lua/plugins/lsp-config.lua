b3bdc3b095b75716ab1ac8d7c5a7518db9270479
Minjares <A01284650@tec.mx>
2024-04-07 13:24:13 -0600

    LSP changes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,53 +1,57 @@
 ---@diagnostic disable: undefined-global
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "basedpyright", "html", "gopls", "cssls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "tsserver", "basedpyright", "html", "gopls", "cssls", "tailwindcss" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
 
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.tsserver.setup({
 
-				capabilities = capabilities,
-			})
-			lspconfig.basedpyright.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.basedpyright.setup({
 
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.html.setup({
 
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.gopls.setup({
 
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.cssls.setup({
 
-				capabilities = capabilities,
-			})
+                capabilities = capabilities,
+            })
+            lspconfig.tailwindcss.setup({
 
-			vim.keymap.set("n", "N", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.type_definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+                capabilities = capabilities,
+            })
+
+            vim.keymap.set("n", "N", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
