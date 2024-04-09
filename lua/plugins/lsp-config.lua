bb6743c7859ac7c1c3460ac1660eb3d70ae76072
Minjares <A01284650@tec.mx>
2024-04-09 10:20:54 -0600

    AutoPair

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -48,7 +48,7 @@ return {
                 capabilities = capabilities,
             })
 
-            vim.keymap.set("n", "N", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
